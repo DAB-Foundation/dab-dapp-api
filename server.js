464e967593dbb38606ac36497ff9c2adc5fa2580
dabdevelop <develop@dab.vc>
2017-08-18 16:19:12 +0800

    Fixed some bugs.

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-PINGOTHER, Content-Type, Accept");
+  res.header("Access-Control-Max-Age", "86400");
   next();
 });
 
